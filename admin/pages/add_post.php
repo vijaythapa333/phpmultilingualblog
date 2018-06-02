3401c78e2e78a3a7967a5e21c5be47d341481cae
Vijay Thapa <vijaythapa333@gmail.com>
2018-06-02 18:03:56 +0545

    Select and Insert Functionality for Posts, Completed Successfully.

---
+++ admin/pages/add_post.php
@@ -0,0 +1,96 @@
+<div class="body">
+	<h2>Add Post</h2>
+	<br>
+	<?php 
+		if(isset($_SESSION['add']))
+		{
+			echo $_SESSION['add'];
+			unset($_SESSION['add']);
+		}
+	?>
+	<form method="post" action="">
+		<div class="input-group">
+			<span class="input-label">Post Title (English)</span>
+			<input class="half" type="text" name="title_en" placeholder="Post Title in English" required="true">
+		</div>
+		<div class="input-group">
+			<span class="input-label">Post Title (Nepali)</span>
+			<input class="half" type="text" name="title_np" placeholder="Post Title in Nepali">
+		</div>
+		<div class="input-group">
+			<span class="input-label">Post Title (Chinese)</span>
+			<input class="half" type="text" name="title_cn" placeholder="Post Title in Chinese">
+		</div>
+
+		<div class="input-group">
+			<span class="input-label">Post Description (English)</span>
+			<textarea class="half" name="description_en" placeholder="Post Description in English" required="true"></textarea>
+		</div>
+		<div class="input-group">
+			<span class="input-label">Post Description (Nepali)</span>
+			<textarea class="half" name="description_np" placeholder="Post Description in English" required="true"></textarea>
+		</div>
+		<div class="input-group">
+			<span class="input-label">Post Description (Chinese)</span>
+			<textarea class="half" name="description_cn" placeholder="Post Description in English" required="true"></textarea>
+		</div>
+
+		<div class="input-group">
+			<span class="input-label">Is Active?</span>
+			<input type="radio" name="is_active" value="Yes"> Yes
+			<input type="radio" name="is_active" value="No"> No
+		</div>
+
+		<div class="input-group">
+			<span class="input-label">Is Featured?</span>
+			<input type="radio" name="is_featured" value="Yes"> Yes
+			<input type="radio" name="is_featured" value="No"> No
+		</div>
+		<br>
+		<div class="input-group">
+			<input class="btn-primary btn-sm" type="submit" name="submit" value="Add Post">
+		</div>
+	</form>
+
+	<?php 
+		if(isset($_POST['submit']))
+		{
+			$title_en = $obj->sanitize($conn,$_POST['title_en']);
+			$title_np = $obj->sanitize($conn,$_POST['title_np']);
+			$title_cn = $obj->sanitize($conn,$_POST['title_cn']);
+			$description_en = $obj->sanitize($conn,$_POST['description_en']);
+			$description_np = $obj->sanitize($conn,$_POST['description_np']);
+			$description_cn = $obj->sanitize($conn,$_POST['description_cn']);
+			$is_active = $_POST['is_active'];
+			$is_featured = $_POST['is_featured'];
+			$created_at = date('Y-m-d H:i:s');
+
+			$data="
+				title_en='$title_en',
+				title_np='$title_np',
+				title_cn='$title_cn',
+				description_en='$description_en',
+				description_np='$description_np',
+				description_cn='$description_cn',
+				is_active='$is_active',
+				is_featured='$is_featured',
+				created_at='$created_at'
+			";
+
+			$tbl_name='tbl_posts';
+			$query = $obj->insert_data($tbl_name,$data);
+			$res = $obj->execute_query($conn,$query);
+
+			if($res == true)
+			{
+				$_SESSION['add'] = "<div class='success'>New Post Successfully Added.</div>";
+				header('location:'.SITEURL.'admin/index.php?page=posts');
+			}
+			else
+			{
+				$_SESSION['add'] = "<div class='error'>Failed to add new post.</div>";
+				header('location:'.SITEURL.'admin/index.php?page=add_post');
+			}
+		}
+	?>
+</div>
--- admin/pages/posts.php
+++ admin/pages/posts.php
@@ -1,6 +1,63 @@
 <div class="body">
 	<h2><?php echo $lang['posts'] ?></h2>
-	<p>
-		You'll Manage Everything from Here.
-	</p>
+	<br>
+	<?php 
+		if(isset($_SESSION['add']))
+		{
+			echo $_SESSION['add'];
+			unset($_SESSION['add']);
+		}
+	?>
+	<table class="tbl-responsive">
+		<tr>
+			<td colspan="5">
+				<a href="<?php echo SITEURL; ?>admin/index.php?page=add_post" class="btn-primary btn-sm">Add Post</a>
+			</td>
+		</tr>
+		<tr>
+			<th>S.N.</th>
+			<th>Post Title</th>
+			<th>Is Active?</th>
+			<th>Is Featured?</th>
+			<th>Actions</th>
+		</tr>
+
+		<?php 
+			$tbl_name = 'tbl_posts';
+			$query = $obj->select_data($tbl_name);
+			$res = $obj->execute_query($conn,$query);
+			$sn = 1;
+
+			if($res==true)
+			{
+				$count_rows = $obj->num_rows($res);
+				if($count_rows>0)
+				{
+					while ($row=$obj->fetch_data($res)) {
+						$id = $row['id'];
+						$title = $row['title_'.$_SESSION['lang']];
+						$is_active = $row['is_active'];
+						$is_featured = $row['is_featured'];
+						?>
+						<tr>
+							<td><?php echo $sn++; ?>. </td>
+							<td><?php echo $title; ?></td>
+							<td><?php echo $is_active; ?></td>
+							<td><?php echo $is_featured; ?></td>
+							<td>
+								<a href="<?php echo SITEURL; ?>admin/index.php?page=edit_post&id=<?php echo $id; ?>" class="btn-success btn-sm">Edit</a> 
+								<a href="#" class="btn-error btn-sm">Delete</a>
+							</td>
+						</tr>
+						<?php
+					}
+				}
+				else
+				{
+					echo"<tr><td colspan='5' class='error'>No Posts Found.</td></tr>";
+				}
+			}
+		?>
+		
+	</table>
 </div>
